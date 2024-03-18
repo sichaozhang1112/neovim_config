b1d7f50b25c584fc1cf572f50dc0d5bcd0a2d966
ganah <schizoganah@gmail.com>
2024-03-18 16:56:34 +0800

    [fix] no need for ignore_buffer_on_setup

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -9,7 +9,6 @@ vim.opt.termguicolors = true
 require("nvim-tree").setup({
         sort_by = "case_sensitive",
         hijack_cursor = true,
-        ignore_buffer_on_setup = true,
         system_open = {
                 cmd = "open",
         },
