9425ab8689643d4af0577060bcd1e4e8507cdd70
ganah <schizoganah@gmail.com>
2026-01-30 15:32:00 +0800

    [chore] update the plugins

--- lua/plugins/opencode.lua
+++ lua/plugins/opencode.lua
@@ -23,7 +23,7 @@ return {
 			vim.keymap.set({ "n", "x" }, "<C-x>", function()
 				require("opencode").select()
 			end, { desc = "Execute opencode action…" })
-			vim.keymap.set({ "n", "t" }, "<C-/>", function()
+			vim.keymap.set({ "n", "t" }, "<C-_>", function()
 				require("opencode").toggle()
 			end, { desc = "Toggle opencode" })
 
