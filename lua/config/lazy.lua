447c6f87b88f178191834614308390d5a6144d0f
GANAH <schizoganah@gmail.com>
2026-02-13 15:18:42 +0800

    [chore] disable lazy rocks

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -32,4 +32,8 @@ require("lazy").setup({
 	install = { colorscheme = { "habamax" } },
 	-- automatically check for plugin updates
 	checker = { enabled = true },
+	rocks = {
+		enabled = false,
+		hererocks = false,
+	},
 })
